5a91dbc4d0c202c64bbd1e1b04074b23b515b86f
Brooke Kuhlmann <brooke@alchemists.io>
2023-01-17 17:31:51 -0700

    Updated RSpec parser shared example to refer to Rubysmith configuration
    Only the Rubysmith configuration is being used at the moment.

--- spec/support/shared_examples/a_parser.rb
+++ spec/support/shared_examples/a_parser.rb
@@ -3,7 +3,7 @@
 RSpec.shared_examples "a parser" do
   describe ".call" do
     it "answers configuration" do
-      expect(described_class.call).to be_a(Hanamismith::Configuration::Content)
+      expect(described_class.call).to be_a(Rubysmith::Configuration::Content)
     end
   end
 end
