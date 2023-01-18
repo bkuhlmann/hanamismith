b1e4d5a3840b91e9c01fcab3bca98779a6f1a4a3
Brooke Kuhlmann <brooke@alchemists.io>
2023-01-17 17:31:52 -0700

    Added Bundler builder
    Necessary to properly configure Hanami dependencies. For readability, code-wise, dependencies are inserted in alphabetical order but show up in reverse order in the template for testing purposes. During project generation, these dependnecies will be correctly alphabetically sorted since Rubysmith will run RuboCop autocorrect.
    
    If no development or test groups exist, then the full group blocks are inserted. This works because the `Rubysmith::Builder#insert_after` regular expression will only fire if a match is found. Otherwise, if the development or test groups already exist, then the dependencies are inserted whole sale.

---
+++ lib/hanamismith/builders/bundler.rb
@@ -0,0 +1,95 @@
+# frozen_string_literal: true
+
+require "refinements/structs"
+
+module Hanamismith
+  module Builders
+    # Builds project skeleton with Gemfile configuration.
+    class Bundler < Rubysmith::Builders::Bundler
+      using Refinements::Structs
+
+      # :reek:TooManyStatements
+      def call
+        super
+        insert_main_dependencies
+        insert_persistence_dependencies
+        alter_groups
+        append_development_group
+        append_test_group
+        remove_zeitwerk
+        configuration
+      end
+
+      private
+
+      attr_reader :configuration, :builder
+
+      # rubocop:todo Metrics/MethodLength
+      def insert_main_dependencies
+        with_template.insert_after("source", %(gem "dry-types", "~> 1.7"))
+                     .insert_after("source", %(gem "erbse", "~> 0.1"\n))
+                     .insert_after("source", %(gem "hanami", "~> 2.0"\n))
+                     .insert_after("source", %(gem "hanami-controller", "~> 2.0"\n))
+                     .insert_after("source", %(gem "hanami-router", "~> 2.0"\n))
+                     .insert_after("source", %(gem "hanami-validations", "~> 2.0"\n))
+                     .insert_after(
+                       "source",
+                       %(gem "hanami-view", github: "hanami/view", branch: "main"\n)
+                     )
+                     .insert_after("source", %(gem "puma", "~> 6.0"\n))
+      end
+      # rubocop:enable Metrics/MethodLength
+
+      def insert_persistence_dependencies
+        with_template.insert_after("source", %(gem "pg", "~> 1.4"\n))
+                     .insert_after("source", %(gem "rom", "~> 5.3"\n))
+                     .insert_after("source", %(gem "rom-sql", "~> 3.6"\n))
+                     .insert_after("source", %(\ngem "sequel", "~> 5.64"\n))
+      end
+
+      def alter_groups
+        with_template.insert_after(/group :code_quality/, %(  gem "rubocop-sequel", "~> 0.3"\n))
+                     .insert_after(/group :development/, %(  gem "localhost", "~> 1.1"\n))
+                     .insert_after(/group :development/, %(  gem "rerun", "~> 0.14"\n))
+                     .insert_after(/group :test/, %(  gem "capybara", "~> 3.38"\n))
+                     .insert_after(/group :test/, %(  gem "cuprite", "~> 0.14"\n))
+                     .insert_after(/group :test/, %(  gem "rack-test", "~> 2.0"\n))
+                     .insert_after(/group :test/, %(  gem "rom-factory", "~> 0.11"\n))
+                     .replace(/gem "rspec.+/, %(gem "hanami-rspec", "~> 2.0"))
+      end
+
+      def append_development_group
+        return if configuration.markdown? || configuration.build_rake || configuration.build_yard
+
+        with_template.append <<~CONTENT
+
+          group :development do
+            gem "localhost", "~> 1.1"
+            gem "rerun", "~> 0.14"
+          end
+
+        CONTENT
+      end
+
+      def append_test_group
+        return if configuration.build_guard || configuration.build_rspec
+
+        with_template.append <<~CONTENT
+          group :test do
+            gem "capybara", "~> 3.38"
+            gem "cuprite", "~> 0.14"
+            gem "hanami-rspec", "~> 2.0"
+            gem "rack-test", "~> 2.0"
+            gem "rom-factory", "~> 0.11"
+          end
+        CONTENT
+      end
+
+      def remove_zeitwerk = with_template.replace(/.+zeitwerk.+\n\n/, "\n")
+
+      def with_template
+        builder.call configuration.merge(template_path: "%project_name%/Gemfile.erb")
+      end
+    end
+  end
+end
---
+++ spec/lib/hanamismith/builders/bundler_spec.rb
@@ -0,0 +1,115 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe Hanamismith::Builders::Bundler do
+  using Refinements::Structs
+
+  subject(:builder) { described_class.new test_configuration }
+
+  include_context "with application dependencies"
+
+  it_behaves_like "a builder"
+
+  describe "#call" do
+    before { builder.call }
+
+    context "with minimum flags" do
+      let(:test_configuration) { configuration.minimize }
+
+      it "updates Gemfile" do
+        expect(temp_dir.join("test", "Gemfile").read).to eq(<<~CONTENT)
+          ruby File.read(".ruby-version").strip
+
+          source "https://rubygems.org"
+
+          gem "sequel", "~> 5.64"
+          gem "rom-sql", "~> 3.6"
+          gem "rom", "~> 5.3"
+          gem "pg", "~> 1.4"
+          gem "puma", "~> 6.0"
+          gem "hanami-view", github: "hanami/view", branch: "main"
+          gem "hanami-validations", "~> 2.0"
+          gem "hanami-router", "~> 2.0"
+          gem "hanami-controller", "~> 2.0"
+          gem "hanami", "~> 2.0"
+          gem "erbse", "~> 0.1"
+          gem "dry-types", "~> 1.7"
+
+          group :development do
+            gem "localhost", "~> 1.1"
+            gem "rerun", "~> 0.14"
+          end
+
+          group :test do
+            gem "capybara", "~> 3.38"
+            gem "cuprite", "~> 0.14"
+            gem "hanami-rspec", "~> 2.0"
+            gem "rack-test", "~> 2.0"
+            gem "rom-factory", "~> 0.11"
+          end
+        CONTENT
+      end
+    end
+
+    context "with maximum flags" do
+      let(:test_configuration) { configuration.maximize }
+
+      let :proof do
+        <<~CONTENT
+          ruby File.read(".ruby-version").strip
+
+          source "https://rubygems.org"
+
+          gem "sequel", "~> 5.64"
+          gem "rom-sql", "~> 3.6"
+          gem "rom", "~> 5.3"
+          gem "pg", "~> 1.4"
+          gem "puma", "~> 6.0"
+          gem "hanami-view", github: "hanami/view", branch: "main"
+          gem "hanami-validations", "~> 2.0"
+          gem "hanami-router", "~> 2.0"
+          gem "hanami-controller", "~> 2.0"
+          gem "hanami", "~> 2.0"
+          gem "erbse", "~> 0.1"
+          gem "dry-types", "~> 1.7"
+          gem "refinements", "~> 10.0"
+
+          group :code_quality do
+            gem "rubocop-sequel", "~> 0.3"
+            gem "caliber", "~> 0.21"
+            gem "git-lint", "~> 5.0"
+            gem "reek", "~> 6.1"
+            gem "simplecov", "~> 0.22", require: false
+          end
+
+          group :development do
+            gem "rerun", "~> 0.14"
+            gem "localhost", "~> 1.1"
+            gem "asciidoctor", "~> 2.0"
+            gem "rake", "~> 13.0"
+            gem "yard", "~> 0.9"
+          end
+
+          group :test do
+            gem "rom-factory", "~> 0.11"
+            gem "rack-test", "~> 2.0"
+            gem "cuprite", "~> 0.14"
+            gem "capybara", "~> 3.38"
+            gem "guard-rspec", "~> 4.7", require: false
+            gem "hanami-rspec", "~> 2.0"
+          end
+
+          group :tools do
+            gem "amazing_print", "~> 1.4"
+            gem "debug", "~> 1.7"
+          end
+        CONTENT
+      end
+
+      it "updates Gemfile" do
+        expect(temp_dir.join("test", "Gemfile").read).to eq(proof)
+      end
+    end
+  end
+end
