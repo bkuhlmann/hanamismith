eedb0e559279e58bebfad556e01009741aad71e7
Brooke Kuhlmann <brooke@alchemists.io>
2023-06-19 10:39:58 -0600

    Updated container to use Etcher configuration
    Necessary to leverage Etcher and Runcom for loading and resolving the configuration.

--- lib/hanamismith/container.rb
+++ lib/hanamismith/container.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 require "cogger"
-require "dry/container"
+require "dry-container"
+require "etcher"
+require "runcom"
 require "spek"
 
 module Hanamismith
@@ -9,7 +11,32 @@ module Hanamismith
   module Container
     extend Dry::Container::Mixin
 
-    register(:configuration) { Configuration::Loader.call }
+    register :configuration do
+      self[:defaults].add_loader(Etcher::Loaders::YAML.new(self[:xdg_config].active))
+                     .then { |registry| Etcher.call registry }
+    end
+
+    register :defaults do
+      registry = Etcher::Registry.new contract: Rubysmith::Configuration::Contract,
+                                      model: Rubysmith::Configuration::Model
+
+      registry.add_loader(Etcher::Loaders::YAML.new(self[:defaults_path]))
+              .add_transformer(Rubysmith::Configuration::Transformers::CurrentTime)
+              .add_transformer(Rubysmith::Configuration::Transformers::GitHubUser.new)
+              .add_transformer(Rubysmith::Configuration::Transformers::GitEmail.new)
+              .add_transformer(Rubysmith::Configuration::Transformers::GitUser.new)
+              .add_transformer(Rubysmith::Configuration::Transformers::TemplateRoot.new)
+              .add_transformer(
+                Rubysmith::Configuration::Transformers::TemplateRoot.new(
+                  Pathname(__dir__).join("templates")
+                )
+              )
+              .add_transformer(Rubysmith::Configuration::Transformers::TargetRoot)
+    end
+
+    register(:input, memoize: true) { self[:configuration].dup }
+    register(:defaults_path) { Rubysmith::Container[:defaults_path] }
+    register(:xdg_config) { Runcom::Config.new "hanamismith/configuration.yml" }
     register(:specification) { Spek::Loader.call "#{__dir__}/../../hanamismith.gemspec" }
     register(:kernel) { Kernel }
     register(:logger) { Cogger.new formatter: :emoji }
--- spec/support/shared_contexts/application_dependencies.rb
+++ spec/support/shared_contexts/application_dependencies.rb
@@ -4,23 +4,30 @@ require "dry/container/stub"
 require "infusible/stub"
 
 RSpec.shared_context "with application dependencies" do
-  using Refinements::Structs
   using Infusible::Stub
 
   include_context "with temporary directory"
 
   let :configuration do
-    Hanamismith::Configuration::Loader.with_overrides.call.merge(
-      now: Time.local(2020, 1, 1, 0, 0, 0),
-      project_name: "test",
-      target_root: temp_dir
-    )
+    Etcher.new(Hanamismith::Container[:defaults])
+          .call(
+            author_family_name: "Smith",
+            author_given_name: "Jill",
+            author_email: "jill@example.com",
+            git_hub_user: "hubber",
+            now: Time.local(2020, 1, 1, 0, 0, 0),
+            project_name: "test",
+            target_root: temp_dir
+          )
+          .bind(&:dup)
   end
 
+  let(:input) { configuration.dup }
+  let(:xdg_config) { Runcom::Config.new Hanamismith::Container[:defaults_path] }
   let(:kernel) { class_spy Kernel }
-  let(:logger) { Cogger.new io: StringIO.new, formatter: :emoji }
+  let(:logger) { Cogger.new io: StringIO.new, level: :debug, formatter: :emoji }
 
-  before { Hanamismith::Import.stub configuration:, kernel:, logger: }
+  before { Hanamismith::Import.stub configuration:, input:, xdg_config:, kernel:, logger: }
 
-  after { Hanamismith::Import.unstub :configuration, :kernel, :logger }
+  after { Hanamismith::Import.unstub :configuration, :input, :xdg_config, :kernel, :logger }
 end
