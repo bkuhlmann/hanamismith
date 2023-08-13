9597a04e9fbaa89e3b28940da5c3def6e9aae23f
Brooke Kuhlmann <brooke@alchemists.io>
2023-08-13 08:21:17 -0600

    Fixed RSpec helper builder to temporarily ignore SimpleCov eval
    Use of SimpleCov eval coverage is recommended but, unfortunately, can't be enabled for Hanami applications due to link:https://github.com/hanami/view[Hanami View] being built atop link:https://github.com/jeremyevans/tilt[Tilt] which causes this link:https://bugs.ruby-lang.org/issues/19363[Ruby Issue]. Until this issue is resoved, use of eval is marked as a pending TODO.
    
    Milestone: patch

--- lib/hanamismith/builders/rspec/helper.rb
+++ lib/hanamismith/builders/rspec/helper.rb
@@ -9,14 +9,37 @@ module Hanamismith
       class Helper < Rubysmith::Builders::RSpec::Helper
         using Refinements::Structs
 
+        def initialize(...)
+          super
+          @instance = builder.call configuration.merge(
+            template_path: "%project_name%/spec/spec_helper.rb.erb"
+          )
+        end
+
         def call
           return configuration unless configuration.build_rspec
 
           super
-          path = "%project_name%/spec/spec_helper.rb.erb"
-          builder.call(configuration.merge(template_path: path))
-                 .replace(/require.+#{configuration.project_name}.+\n/, "")
+          remove_project_requirement
+          disable_simple_cov_eval
           configuration
+        end
+
+        private
+
+        attr_reader :instance
+
+        def remove_project_requirement
+          instance.replace(/require.+#{configuration.project_name}.+\n/, "")
+        end
+
+        def disable_simple_cov_eval
+          instance.replace(
+            /enable_coverage_for_eval\n/,
+            "# TODO: Enable once this issue is fixed: " \
+            "https://bugs.ruby-lang.org/issues/19363.\n    " \
+            "# enable_coverage_for_eval\n"
+          )
         end
       end
     end
--- spec/lib/hanamismith/builders/rspec/helper_spec.rb
+++ spec/lib/hanamismith/builders/rspec/helper_spec.rb
@@ -12,13 +12,15 @@ RSpec.describe Hanamismith::Builders::RSpec::Helper do
   it_behaves_like "a builder"
 
   describe "#call" do
+    let(:spec_helper_path) { temp_dir.join "test/spec/spec_helper.rb" }
+
     before { builder.call }
 
     context "when enabled" do
       let(:test_configuration) { configuration.minimize.merge build_rspec: true }
 
       it "adds helper" do
-        expect(temp_dir.join("test/spec/spec_helper.rb").read).to eq(<<~CONTENT)
+        expect(spec_helper_path.read).to eq(<<~CONTENT)
           Bundler.require :tools
 
 
@@ -47,6 +49,60 @@ RSpec.describe Hanamismith::Builders::RSpec::Helper do
             end
           end
         CONTENT
+      end
+    end
+
+    context "when enabled with SimpleCov only" do
+      let :test_configuration do
+        configuration.minimize.merge build_rspec: true, build_simple_cov: true
+      end
+
+      let :proof do
+        <<~BODY
+          require "simplecov"
+
+          unless ENV["NO_COVERAGE"]
+            SimpleCov.start do
+              add_filter %r(^/spec/)
+              enable_coverage :branch
+              # TODO: Enable once this issue is fixed: https://bugs.ruby-lang.org/issues/19363.
+              # enable_coverage_for_eval
+              minimum_coverage_by_file line: 95, branch: 95
+            end
+          end
+
+          Bundler.require :tools
+
+
+          SPEC_ROOT = Pathname(__dir__).realpath.freeze
+
+          Dir[File.join(SPEC_ROOT, "support", "shared_contexts", "**/*.rb")].each { |path| require path }
+
+          RSpec.configure do |config|
+            config.color = true
+            config.disable_monkey_patching!
+            config.example_status_persistence_file_path = "./tmp/rspec-examples.txt"
+            config.filter_run_when_matching :focus
+            config.formatter = ENV.fetch("CI", false) == "true" ? :progress : :documentation
+            config.order = :random
+            config.shared_context_metadata_behavior = :apply_to_host_groups
+            config.warnings = true
+
+            config.expect_with :rspec do |expectations|
+              expectations.syntax = :expect
+              expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+            end
+
+            config.mock_with :rspec do |mocks|
+              mocks.verify_doubled_constant_names = true
+              mocks.verify_partial_doubles = true
+            end
+          end
+        BODY
+      end
+
+      it "builds spec helper" do
+        expect(spec_helper_path.read).to eq(proof)
       end
     end
 
