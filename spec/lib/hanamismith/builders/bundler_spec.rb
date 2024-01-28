7c95e30e5877792906aa0ef463b6a4bb34c19ae6
Brooke Kuhlmann <brooke@alchemists.io>
2024-01-28 07:23:25 -0700

    Updated to Caliber 0.50.0
    See link:https://alchemists.io/projects/caliber/versions[release notes] for details.
    
    Milestone: minor

--- spec/lib/hanamismith/builders/bundler_spec.rb
+++ spec/lib/hanamismith/builders/bundler_spec.rb
@@ -96,7 +96,7 @@ RSpec.describe Hanamismith::Builders::Bundler do
 
           group :quality do
             gem "rubocop-sequel", "~> 0.3"
-            gem "caliber", "~> 0.47"
+            gem "caliber", "~> 0.50"
             gem "git-lint", "~> 7.0"
             gem "reek", "~> 6.3", require: false
             gem "simplecov", "~> 0.22", require: false
