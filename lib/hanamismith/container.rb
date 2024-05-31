aeb72bda29a59f4c24e49e3d2e49b4d30e231166
Brooke Kuhlmann <brooke@alchemists.io>
2024-05-31 11:34:30 -0600

    Updated to Etcher 1.6.0
    See link:https://alchemists.io/projects/etcher/versions[details].
    
    Milestone: minor

--- lib/hanamismith/container.rb
+++ lib/hanamismith/container.rb
@@ -12,7 +12,7 @@ module Hanamismith
     extend Containable
 
     register :configuration do
-      self[:defaults].add_loader(Etcher::Loaders::YAML.new(self[:xdg_config].active))
+      self[:defaults].add_loader(:yaml, self[:xdg_config].active)
                      .then { |registry| Etcher.call registry }
     end
 
@@ -20,7 +20,7 @@ module Hanamismith
       registry = Etcher::Registry.new contract: Rubysmith::Configuration::Contract,
                                       model: Rubysmith::Configuration::Model
 
-      registry.add_loader(Etcher::Loaders::YAML.new(self[:defaults_path]))
+      registry.add_loader(:yaml, self[:defaults_path])
               .add_transformer(Rubysmith::Configuration::Transformers::GitHubUser.new)
               .add_transformer(Rubysmith::Configuration::Transformers::GitEmail.new)
               .add_transformer(Rubysmith::Configuration::Transformers::GitUser.new)
@@ -31,7 +31,7 @@ module Hanamismith
                 )
               )
               .add_transformer(Rubysmith::Configuration::Transformers::TargetRoot)
-              .add_transformer(Etcher::Transformers::Time.new)
+              .add_transformer(:time)
     end
 
     register(:specification) { Spek::Loader.call "#{__dir__}/../../hanamismith.gemspec" }
