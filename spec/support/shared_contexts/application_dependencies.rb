e7f074918f331a5d8c02d87de3793ac1fede106d
Brooke Kuhlmann <brooke@alchemists.io>
2024-06-02 15:31:58 -0600

    Updated to Etcher 2.1.0
    See link:https://alchemists.io/projects/etcher/versions[details].
    
    Milestone: major

--- spec/support/shared_contexts/application_dependencies.rb
+++ spec/support/shared_contexts/application_dependencies.rb
@@ -1,25 +1,26 @@
 # frozen_string_literal: true
 
 RSpec.shared_context "with application dependencies" do
+  using Refinements::Struct
+
   include_context "with temporary directory"
 
-  let :configuration do
-    Etcher.new(Hanamismith::Container[:defaults])
-          .call(
-            author_family_name: "Smith",
-            author_given_name: "Jill",
-            project_name: "test",
-            target_root: temp_dir
-          )
-          .bind(&:dup)
-  end
-
-  let(:input) { configuration.dup }
+  let(:settings) { Hanamismith::Container[:settings] }
   let(:xdg_config) { Runcom::Config.new Hanamismith::Container[:defaults_path] }
   let(:kernel) { class_spy Kernel }
   let(:logger) { Cogger.new id: :hanamismith, io: StringIO.new, level: :debug }
 
-  before { Hanamismith::Container.stub! configuration:, input:, xdg_config:, kernel:, logger: }
+  before do
+    settings.merge! Etcher.call(
+      Hanamismith::Container[:registry].remove_loader(1),
+      author_family_name: "Smith",
+      author_given_name: "Jill",
+      project_name: "test",
+      target_root: temp_dir
+    )
+
+    Hanamismith::Container.stub! xdg_config:, kernel:, logger:
+  end
 
   after { Hanamismith::Container.restore }
 end
