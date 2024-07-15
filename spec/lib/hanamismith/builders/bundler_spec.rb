b2a65e0362914603181eef45b3166758af07e066
Brooke Kuhlmann <brooke@alchemists.io>
2024-07-15 16:45:39 -0600

    Updated to Refinements 12.7.0
    See link:https://alchemists.io/projects/refinements/versions[details].
    
    Milestone: minor

--- spec/lib/hanamismith/builders/bundler_spec.rb
+++ spec/lib/hanamismith/builders/bundler_spec.rb
@@ -89,7 +89,7 @@ RSpec.describe Hanamismith::Builders::Bundler do
           gem "hanami-router", "~> 2.1"
           gem "hanami-validations", "~> 2.1"
           gem "hanami-view", "~> 2.1"
-          gem "refinements", "~> 12.5"
+          gem "refinements", "~> 12.7"
 
           group :quality do
             gem "rubocop-sequel", "~> 0.3"
