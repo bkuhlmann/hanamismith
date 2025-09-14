aba058be7382120850220e30e7c05ed63fa5feb1
Brooke Kuhlmann <brooke@alchemists.io>
2025-09-14 15:36:48 -0600

    Added layout builder flash alerts and notices
    Necessary to provide default flash alert and notice messages. The tag helpers were also updated to use consistent syntax.
    
    RuboCop was updated to ignore the metric issues because we need to keep all of this logic together. Breaking this up further would not be helpful in this case.
    
    Milestone: minor

--- lib/hanamismith/builders/layout.rb
+++ lib/hanamismith/builders/layout.rb
@@ -13,21 +13,53 @@ module Hanamismith
         builder.call(settings.with(template_path: path))
                .render
                .replace("<!-- title -->", "<%= content_for :title %>")
+               .replace("<!-- favicon -->", favicon)
                .replace("<!-- icon -->", icon)
+               .replace("<!-- apple_icon -->", apple_icon)
                .replace("<!-- manifest -->", manifest)
                .replace("<!-- stylesheet -->", stylesheet)
+               .replace("<!-- javascript -->", javascript)
                .replace("<!-- yield -->", "<%= yield %>")
+               .replace("<!-- flash:alert -->", flash(:alert))
+               .replace("<!-- flash:notice -->", flash(:notice))
 
         true
       end
 
       private
 
+      def favicon
+        uri = "https://alchemists.io/images/projects/hanamismith/icons/favicon.ico"
+
+        %(<%= tag.link title: "#{settings.project_label}: Favicon",\n) +
+          %(                 rel: :icon,\n) +
+          %(                 href: "#{uri}",\n) +
+          %(                 sizes: "32x32" %>)
+      end
+
       def icon
         %(<%= tag.link title: "#{settings.project_label}: Icon",\n) +
           %(                 rel: :icon,\n) +
           %(                 href: app_assets["icon.svg"],\n) +
           %(                 type: "image/svg+xml" %>)
+      end
+
+      def apple_icon
+        uri = "https://alchemists.io/images/projects/hanamismith/icons/apple.png"
+
+        %(<%= tag.link title: "#{settings.project_label}: Apple Icon",\n) +
+          %(                 rel: "apple-touch-icon",\n) +
+          %(                 href: "#{uri}",\n) +
+          %(                 type: "image/png" %>)
+      end
+
+      # :reek:UtilityFunction
+      def flash kind
+        %(<% if flash[:#{kind}] %>\n) +
+          %(      <div class="site-#{kind}">\n) +
+          %(        <p><%= flash[:#{kind}] %></p>\n) +
+          %(      </div>\n) +
+          %(    <% end %>\n)
       end
 
       def manifest
@@ -40,6 +72,8 @@ module Hanamismith
         %(<%= tag.link title: "#{settings.project_label}: Stylesheet", rel: :stylesheet, ) +
           %(href: app_assets["app.css"] %>)
       end
+
+      def javascript = %(<%= tag.script src: app_assets["app.js"], type: "text/javascript" %>)
     end
   end
 end
