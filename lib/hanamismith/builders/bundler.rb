0db716bfbc9581e0f00e8d84f0deb13676c2eebe
Brooke Kuhlmann <brooke@alchemists.io>
2026-01-09 08:17:12 -0700

    Updated to Dry Types 1.9.0
    link:https://github.com/dry-rb/dry-types/blob/main/CHANGELOG.md#190---2026-01-09[Details].
    
    Milestone: minor

--- lib/hanamismith/builders/bundler.rb
+++ lib/hanamismith/builders/bundler.rb
@@ -48,7 +48,7 @@ module Hanamismith
       def insert_main
         with_template.insert_after "source", <<~CONTENT
           gem "dry-schema", "~> 1.14"
-          gem "dry-types", "~> 1.8"
+          gem "dry-types", "~> 1.9"
           gem "dry-validation", "~> 1.10"
           gem "htmx", "~> 3.0"
           gem "overmind", "~> 2.5"
